543f5631806f50fad5c73a56830d347b4a08319a
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2021-12-28 13:13:27 +0300

    added 1.4 1.5 1.6

--- Lab1.1/Lab1.1/Program.cs
+++ Lab1.1/Lab1.1/Program.cs
@@ -134,6 +134,34 @@ namespace lab1_1
             return true;
         }
 
+        bool NextSoch(int c)
+        {
+            int k = c;
+            for (int i = k - 1; i >= 0; i--)
+                if (obj[i] < _n - k + i )
+                {
+                    obj[i]++;
+                    for (int j = i + 1; j < k; j++)
+                        obj[j] = obj[j - 1]+1 ;
+                    return true;
+                }
+            return false;
+        }
+        bool NextSochSPovt()
+        {
+            int j = _k - 1;
+            while ( j >= 0 && obj[j] == _n - 1) j--;
+            if (j < 0) return false;
+            if (obj[j] >= _n-1)
+                j--;
+            obj[j]++;
+            if (j == _k - 1) return true;
+            for (int k = j + 1; k < _k; k++)
+                obj[k] = obj[j];
+            return true;
+        }
+
+
 
         static void Main(string[] args)
         {
@@ -144,7 +172,7 @@ namespace lab1_1
             Program obj = new Program(n, k);
             Program obj2 = new Program(n, n);
             Program obj3 = new Program(n, k, 0);
-            Program obj4 = new Program(n, k);
+            Program obj4 = new Program(n, n);
             Program obj5 = new Program(n, k);
             Program obj6 = new Program(n, k);
             for (int i = 0; i < n; i++)
@@ -161,9 +189,9 @@ namespace lab1_1
             for (int i = 0; i < obj._k; i++)
             {
                 obj.obj[i] = 0;
-            } 
-            StreamWriter sw = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list1.txt");                                          //1
-
+            }
+            /*StreamWriter sw = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list1.txt");*/                                          //1
+            StreamWriter sw = new StreamWriter(@"D:\dm2021\dm2021\Lab1.1\Lab1.1\list1.txt");
             sw.Write("n = " +n+ " k = "+k+ " kolvo strok = n^k = " +Math.Pow(obj._n,obj._k) + " alphabet = {");
 
             for (int i =0;i<n;i++)
@@ -190,8 +218,8 @@ namespace lab1_1
             }
             sw.Close();
 
-            StreamWriter sw2 = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list2.txt");                       //2
-
+            /* StreamWriter sw2 = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list2.txt");*/                       //2
+            StreamWriter sw2 = new StreamWriter(@"D:\dm2021\dm2021\Lab1.1\Lab1.1\list2.txt");
             for (int i = 0; i < obj2._k; i++)
             {
                 obj2.obj[i] = i;
@@ -241,8 +269,8 @@ namespace lab1_1
                 //{
                 //    obj3.obj[i] = i;
                 //}
-                StreamWriter sw3 = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list3.txt");                           //3
-
+                /* StreamWriter sw3 = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list3.txt");*/                           //3
+                StreamWriter sw3 = new StreamWriter(@"D:\dm2021\dm2021\Lab1.1\Lab1.1\list3.txt");
                 c = 1;
                 for (int i = 1; i < n + 1; i++)
                 {
@@ -286,6 +314,152 @@ namespace lab1_1
 
                 sw3.Close();
             }
+
+
+            //StreamWriter sw5 = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list5.txt");
+            StreamWriter sw5 = new StreamWriter(@"D:\dm2021\dm2021\Lab1.1\Lab1.1\list5.txt");
+
+            c = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                c *= i;
+            }
+            c1 = 1;
+            for (int i = 1; i < (n - k) + 1; i++)
+            {
+                c1 *= i;
+            }
+            int c2 = 1;
+            for (int i = 1; i < k + 1; i++)
+            {
+                c2 *= i;
+            }
+
+            sw5.Write("n = " + n + " k = " + k + " kolvo strok = n!/((n-k)!*k!) = " + c/(c1*c2) + " alphabet = {");
+
+            for (int i = 0; i < n; i++)
+            {
+                sw5.Write(obj5.alphabet[i] + " ");
+            }
+            sw5.WriteLine("}");
+
+            for(int i = 0; i < k;i++)
+            {
+                obj5.obj[i] = i;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                sw5.Write(obj5.alphabet[obj5.obj[i]]);
+
+            }
+            sw5.WriteLine();
+
+            while (obj5.NextSoch(obj5._k)) //Вывод перестановок с повт
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    sw5.Write(obj5.alphabet[obj5.obj[i]]);
+
+                }
+                sw5.WriteLine();
+
+            }
+            
+            sw5.Close();
+
+
+            //StreamWriter sw4 = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list4.txt");
+            StreamWriter sw4 = new StreamWriter(@"D:\dm2021\dm2021\Lab1.1\Lab1.1\list4.txt");
+
+            
+
+            sw4.Write("n = " + n +  " kolvo strok = 2^n = " + Math.Pow(2,n) + " alphabet = {");
+
+            for (int i = 0; i < n; i++)
+            {
+                sw4.Write(obj4.alphabet[i] + " ");
+            }
+            sw4.WriteLine("}");
+            for (int l = 0; l < n+1; l++)
+            {
+                for (int i = 0; i < l; i++)
+                {
+                    obj4.obj[i] = i;
+                }
+
+                for (int i = 0; i <l; i++)
+                {
+                    sw4.Write(obj4.alphabet[obj4.obj[i]]);
+
+                }
+                sw4.WriteLine();
+
+                while (obj4.NextSoch(l)) //Вывод перестановок с повт
+                {
+                    for (int i = 0; i < l; i++)
+                    {
+                        sw4.Write(obj4.alphabet[obj4.obj[i]]);
+
+                    }
+                    sw4.WriteLine();
+
+                }
+            }
+            sw4.Close();
+
+
+            //StreamWriter sw5 = new StreamWriter(@"C:\dm2021\Lab1.1\Lab1.1\list5.txt");
+            StreamWriter sw6 = new StreamWriter(@"D:\dm2021\dm2021\Lab1.1\Lab1.1\list6.txt");
+
+            c = 1;
+            for (int i = 1; i < n; i++)
+            {
+                c *= i;
+            }
+            c1 = 1;
+            for (int i = 1; i < (n + k) ; i++)
+            {
+                c1 *= i;
+            }
+            c2 = 1;
+            for (int i = 1; i < k + 1; i++)
+            {
+                c2 *= i;
+            }
+
+            sw6.Write("n = " + n + " k = " + k + " kolvo strok = (n+k-1)!/((n-1)! * k!) = " + c1 / (c * c2) + " alphabet = {");
+
+            for (int i = 0; i < n; i++)
+            {
+                sw6.Write(obj6.alphabet[i] + " ");
+            }
+            sw6.WriteLine("}");
+
+            for (int i = 0; i < k; i++)
+            {
+                obj6.obj[i] = 0;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                sw6.Write(obj6.alphabet[obj6.obj[i]]);
+
+            }
+            sw6.WriteLine();
+
+            while (obj6.NextSochSPovt()) //Вывод перестановок с повт
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    sw6.Write(obj6.alphabet[obj6.obj[i]]);
+
+                }
+                sw6.WriteLine();
+
+            }
+
+            sw6.Close();
 
             Console.WriteLine("1");
         }
